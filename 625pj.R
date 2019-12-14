00858ebf8aa18fb3969e313b9adef94b7a4b0a44
schi006 <sunyichi@umich.edu>
2019-12-13 20:58:31 -0500

    secondsto minutes

--- 625pj.R
+++ 625pj.R
@@ -1,12 +1,15 @@
-readRDS("~/Downloads/workingdata.rds")
+trips=readRDS("/Users/sunyichi/Documents/GitHub/divvy/workingdata_normal.rds")
+trips$tripduration=trips$tripduration/60
+
+
 library(caret)
 library(doParallel)
 cl <- makePSOCKcluster(4)
 registerDoParallel(cl)
-
+trips=trips[,-7]
 ## All subsequent models are then run in parallel
-model <- train(tripduration ~ ., data = trips[1:100000,], method = "rf")
-
+model <- train(tripduration ~ ., data = trips[1:100,], method = "rf")
+saveRDS(trips,file="/Users/sunyichi/Documents/GitHub/divvy/workingdata.rds")
 ## When you are done:
 stopCluster(cl)
 
