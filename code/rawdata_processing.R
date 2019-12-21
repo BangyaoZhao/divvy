508a029f8f85474250b3bd1363765456c8f13bdf
Bob <byzhao@umich.edu>
2019-12-21 13:53:35 -0500

    bob

--- code/rawdata_processing.R
+++ code/rawdata_processing.R
@@ -12,7 +12,7 @@ data$tripduration = as.numeric(data$tripduration)
 data$age = 2018 - as.numeric(data$birthyear)
 
 #discard missing data
-data = filter(data, !is.na(age) & !is.na(gender) & gender != "")
+data = filter(data,!is.na(age) & !is.na(gender) & gender != "")
 
 #discard abnormal data
 data = filter(data, tripduration >= 30)
@@ -28,8 +28,9 @@ data = filter(data, age <= 75)
 #----
 #combine information
 divvydata <- read.csv("data/Divvy_Bicycle_Stations.csv")
-divvydata2 = divvydata[,c("ID","Docks.in.Service")]
-data <- merge(data, divvydata2, by.x = "from_station_id", by.y = "ID")
+divvydata2 = divvydata[, c("ID", "Docks.in.Service")]
+data <-
+  merge(data, divvydata2, by.x = "from_station_id", by.y = "ID")
 # length(unique(data$from_station_id))
 # 602
 
@@ -64,22 +65,27 @@ day_of_week = 1 * (day_of_week == "Monday") +
 data$day_of_week = day_of_week
 
 #convert the tripduration to mins
-data$tripduration=data$tripduration/60
+data$tripduration = data$tripduration / 60
 
 
 #----
 #create cluster
 # divvydata <- read.csv("data/Divvy_Bicycle_Stations.csv")
-loc <- divvydata[, c("ID", "Latitude", "Longitude")] # unique locations the stations
-rownames(loc) <- loc[,1]
+loc <-
+  divvydata[, c("ID", "Latitude", "Longitude")] # unique locations the stations
+rownames(loc) <- loc[, 1]
 loc <- loc[, 2:3]
 
-cl10 <- sapply(seq(10, 100, 10), function(x){ # 10 to 100
+cl10 <- sapply(seq(10, 100, 10), function(x) {
+  # 10 to 100
   c <- kmeans(loc, x, iter.max = 20, nstart = 25)
   return(c$tot.withinss)                      # get total within cluster variance
 })
 png("plots/totwithinss.png")                  # choose K
-plot(seq(10, 100, 10), cl10, type = "l", xlab = "number of clusters",
+plot(seq(10, 100, 10),
+     cl10,
+     type = "l",
+     xlab = "number of clusters",
      ylab = "total within variance")
 dev.off()
 
@@ -87,19 +93,22 @@ dev.off()
 cl40 <- kmeans(loc, 40, iter.max = 20, nstart = 25)
 all.equal(rownames(loc), names(cl40$cluster)) # TRUE
 loc_new <- cbind(ID = rownames(loc), loc, clusterID = cl40$cluster)
-divvydata <- merge(divvydata, cbind(ID = rownames(loc), clusterID = cl40$cluster), 
-                                    by = "ID")
+divvydata <-
+  merge(divvydata, cbind(ID = rownames(loc), clusterID = cl40$cluster),
+        by = "ID")
 write.csv(divvydata, "data/Divvy_Bicycle_Stations.csv", na = "")
 data <- merge(data, loc_new, by.x = "from_station_id", by.y = "ID")
 
-
-
+#----
+#unfactor
+data$usertype = as.numeric(data$usertype)
+data$gender = as.numeric(data$gender)
 
 #----
 #divide data into train and test
 set.seed(1519)
-trains <- sample(1:nrow(data), floor(nrow(data)/2))
-workingdata_train <- data[trains, ]
-workingdata_test <- data[!(1:nrow(data) %in% trains), ]
+trains <- sample(1:nrow(data), floor(nrow(data) / 2))
+workingdata_train <- data[trains,]
+workingdata_test <- data[!(1:nrow(data) %in% trains),]
 saveRDS(workingdata_train, file = "data/workingdata_train.rds")
 saveRDS(workingdata_test, file = "data/workingdata_test.rds")
