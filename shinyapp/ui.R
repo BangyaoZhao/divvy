39d3ea6d4be844f981378dea75f2e1004c201512
schi006 <sunyichi@umich.edu>
2019-12-20 19:08:17 -0500

    shiny app

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -1,16 +1,73 @@
 library(shiny)
+library(ggplot2)
 library(ggrepel)
 library(tidyverse)
-
-
+library(shinydashboard)
+library(shinythemes)
+stations=read.csv("Divvy_Bicycle_Stations.csv")
 ui <- fluidPage(
   
-  fluidPage(
-    
-    titlePanel("Divvy")
-    
+    titlePanel("Divvy"),
+    sidebarLayout(
+      sidebarPanel(
+        fluidRow(
+        box(title = "Input Data",status = "primary",width = 14,
+        selectInput(
+          "Usertype", "Usertype",
+          c(Subscriber = "Subscriber",
+            Customer = "Customer"),
+          selected = "Subscriber"
+        ),
+        selectInput(
+          "Gender", "Gender",
+          c(Male = "Male",
+            Female = "Female"),
+          selected = "Female"
+        ),
+        selectInput(
+          "Season", "Season",
+          c(Spring = "Spring",
+            Summer = "Summer",
+            Autumn="Autumn",
+            Winter="Winter"
+            ),
+          selected = "Spring"
+        ),
+        selectInput(
+          "day", "Which day in the week",
+          c(Monday = "Monday",
+            Tuesday = "Tuesday",
+            Wednesday="Wednesday",
+            Thursday="Thursday",
+            Friday="Friday",
+            Saturday="Saturday",
+            Sunday="Sunday"
+          ),
+          selected = "Spring"
+        ),
+        selectInput(
+          "Station", "Station",
+          c(stationn),
+          selected = "Buckingham Fountain"
+        ),
+        sliderInput(inputId = "age",
+                    label = "Age:",
+                    min = 1,
+                    max = 80,
+                    value = 1)
+        
+        )
+        )
+      ),
+      mainPanel(
+        img(src="mapbox.png"),
+        fluidRow(
+          box(title = "Predicted duration of user using bike",status = "primary",width = 14
+              ))
+      )
+      )
   )
-)
+
 
 
 server <- function(input, output) {
