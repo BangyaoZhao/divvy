b093003b8ef333ee92fdf4cb87656db6c0618350
dxm_ruc@163.com <XuemeiDing@users.noreply.github.com>
2019-12-22 08:10:06 -0500

    seperate for knn

--- cluster/prediction_knn.sh
+++ cluster/prediction_knn.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=xmding@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mem=50GB
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=40
 #SBATCH --array=5
 
 R CMD BATCH --no-save --no-restore prediction_nonP.R prediction$SLURM_ARRAY_TASK_ID.Rout
