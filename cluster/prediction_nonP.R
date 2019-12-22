81276713c4ed0f587072f2cc4120d8e89f4e2939
dxm_ruc@163.com <XuemeiDing@users.noreply.github.com>
2019-12-21 20:40:43 -0500

    prediction code

---
+++ cluster/prediction_nonP.R
@@ -0,0 +1,16 @@
+# read in data and model
+load(rpart.RData)
+workingdata_test <- readRDS("../data/workingdata_test.rds")
+
+# parallel setting
+library(doParallel)
+cl <- makePSOCKcluster(20)
+registerDoParallel(cl)
+
+# predict and calculate MSE
+p <- predict(model, workingdata_test)
+mse <- sum((workingdata_test$tripduration - p)^2 / 1501320)
+
+stopCluster(cl)
+
+save.image("rpart_p.Rdata")
